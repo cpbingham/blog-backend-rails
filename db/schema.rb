df4d637fedcfa3ecf65d694fc5ef2c1733f6fb07
cpbingham <cbingham96@gmail.com>
2024-01-16 20:00:06 -0500

    Adding user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_16_235407) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_17_004951) do
   create_table "posts", charset: "latin1", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "tests", charset: "latin1", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "latin1", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "salt"
+    t.text "hash"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "posts", "users"
 end
